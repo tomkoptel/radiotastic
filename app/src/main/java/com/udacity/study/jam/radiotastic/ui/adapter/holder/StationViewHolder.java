3868f4ac47a59ef614be544f228c64e0b03fda00
Tom Koptel <tom.koptel@gmail.com>
2015-03-27 23:40:23 +0200

    Improving stations list design

--- app/src/main/java/com/udacity/study/jam/radiotastic/ui/adapter/holder/StationViewHolder.java
+++ app/src/main/java/com/udacity/study/jam/radiotastic/ui/adapter/holder/StationViewHolder.java
@@ -8,28 +8,43 @@
 
 package com.udacity.study.jam.radiotastic.ui.adapter.holder;
 
+import android.content.res.Resources;
 import android.database.Cursor;
+import android.util.TypedValue;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.udacity.study.jam.radiotastic.R;
 import com.udacity.study.jam.radiotastic.db.station.StationCursor;
 import com.udacity.study.jam.radiotastic.ui.adapter.easy.EasyViewHolder;
 
 public class StationViewHolder extends EasyViewHolder<Cursor> {
     private final TextView labelTextView;
-    private final TextView descTextView;
+    private final ImageView iconView;
+    private final ColorGenerator mGenerator;
+    private final int px;
 
     public StationViewHolder(ViewGroup parent) {
-        super(parent, R.layout.two_line_list_item);
+        super(parent, R.layout.station_line_list_item);
+        Resources resources = parent.getContext().getResources();
+        px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                40, resources.getDisplayMetrics());
+
         labelTextView = (TextView) itemView.findViewById(android.R.id.text1);
-        descTextView = (TextView) itemView.findViewById(android.R.id.text2);
+        iconView = (ImageView) itemView.findViewById(android.R.id.icon);
+
+        mGenerator = ColorGenerator.DEFAULT;
     }
 
     @Override
     public void bindTo(Cursor cursor) {
         StationCursor item = new StationCursor(cursor);
         labelTextView.setText(item.getName());
-        descTextView.setText(item.getBitrate());
+        TextDrawable textDrawable = TextDrawable.builder()
+                .buildRoundRect(item.getBitrate(), mGenerator.getColor(item.getBitrate()), px);
+        iconView.setImageDrawable(textDrawable);
     }
 }
