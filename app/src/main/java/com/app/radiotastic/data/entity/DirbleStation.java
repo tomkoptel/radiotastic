b01e335ecd3035aa730d88463d65b5f71d1e4c40
Tom Koptel <tom.koptel@gmail.com>
2015-11-02 23:06:17 +0200

    Add StoreIO dependency

--- app/src/main/java/com/app/radiotastic/data/entity/DirbleStation.java
+++ app/src/main/java/com/app/radiotastic/data/entity/DirbleStation.java
@@ -1,16 +1,43 @@
 package com.app.radiotastic.data.entity;
 
+import com.pushtorefresh.storio.sqlite.annotations.StorIOSQLiteColumn;
+import com.pushtorefresh.storio.sqlite.annotations.StorIOSQLiteType;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 /**
  * @author Tom Koptel
  */
+@StorIOSQLiteType(table = "stations")
 public class DirbleStation {
-    private String label;
 
-    public String getLabel() {
-        return label;
+    @Nullable
+    @StorIOSQLiteColumn(name = "_id", key = true)
+    Long id;
+
+    @NotNull
+    @StorIOSQLiteColumn(name = "name")
+    String name;
+
+    // We leave this for StoreIO needs
+    public DirbleStation() {}
+
+    @Nullable
+    public Long getId() {
+        return id;
     }
 
-    public void setLabel(String label) {
-        this.label = label;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
     }
 }
