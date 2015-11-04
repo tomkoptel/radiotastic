53c23a9b1d497f4edf9cf5099a86d3b1ece97cc6
Tom Koptel <tom.koptel@gmail.com>
2015-11-04 14:20:04 +0200

    Lazy initialization of AppComponent

--- app/src/main/java/com/app/radiotastic/App.java
+++ app/src/main/java/com/app/radiotastic/App.java
@@ -6,23 +6,33 @@ import com.app.radiotastic.internal.di.components.AppComponent;
 import com.app.radiotastic.internal.di.components.DaggerAppComponent;
 import com.app.radiotastic.internal.di.modules.AppModule;
 
-public class App extends Application {
+public final class App extends Application {
     private AppComponent mComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        mComponent = DaggerAppComponent.builder()
-                .appModule(new AppModule(this))
-                .build();
     }
 
     public AppComponent getComponent() {
+        if (mComponent == null) {
+            synchronized (App.class) {
+                if (mComponent == null) {
+                    mComponent = createDefaultComponent();
+                }
+            }
+        }
+
         return mComponent;
     }
 
     public void setComponent(AppComponent component) {
         mComponent = component;
+    }
+
+    private AppComponent createDefaultComponent() {
+        return DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
     }
 }
